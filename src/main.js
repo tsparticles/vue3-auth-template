0d5e5bce29eba3cbd5f89853999c862a1ac969b3
Matteo Bruni <ar3s@icloud.com>
2020-12-01 11:29:05 +0100

    changed vue-router history

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import App from './App.vue'
 import Home from './components/Home';
 import Login from './components/Login';
@@ -13,7 +13,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
 
