52d2581dc92923599f54225d9ababa727d64246e
Matteo Bruni <ar3s@icloud.com>
2020-12-01 11:11:35 +0100

    updated app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
+  <div id="main">
     <router-view></router-view>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
 <style>
-#app {
+#main {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
